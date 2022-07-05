31bdcc66cd2bd7259e9f98703296386badd9efae
sujonphero <sujonsheikh.in@gmail.com>
2022-07-04 23:31:17 -0500

    finished

--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -12,11 +12,12 @@ const Countries = () => {
     },[])
 
   return (
-    <section className='bg-blue-200 px-8'>
+    <section className='bg-blue-200 px-8 py-10'>
     <h1 className='text-center text-3xl font-semibold text-slate-500 py-10'>Let's go we are visit : {countries.length}</h1>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
             {
                 countries.map(city => <SingleCountry 
+                key={city.name.common}
                 city={city}
                 ></SingleCountry>)
             }
--- src/Components/SingleCountry.js
+++ src/Components/SingleCountry.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const SingleCountry = (props) => {
-  console.log(props)
+const SingleCountry = ({city}) => {
+  const {name, flags, population} = city 
   return (
     <div className='bg-slate-200 p-4 rounded-md flex flex-col justify-center items-center shadow-md shadow-green-300 hover:scale-105 duration-300'>
       
-        <img className='w-56 h-40' src={props.city.flags.png} alt="" />
-        <h1>Country: {props.city.name.common}</h1>
-        <p>Population: {props.city.population}</p>
+        <img className='w-56 h-40 rounded mb-1' src={flags.png} alt="" />
+        <h1 className='text-lg'>Country: <span className='text-xl text-blue-600'>{name.common}</span></h1>
+
+        <p className='text-lg'>Population: <span className='text-2xl text-orange-600'>{population}</span></p>
     </div>
   )
 }
