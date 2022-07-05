340d6d0e6deab8719c41fac999c6a4c0bf558992
sujonphero <sujonsheikh.in@gmail.com>
2022-07-04 23:17:00 -0500

    done

--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SingleCountry from './SingleCountry'
 
 const Countries = () => {
+    const [countries, setCountries] = useState ([])
+
+    useEffect(()=>{
+        const url = 'https://restcountries.com/v3.1/all'
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    },[])
+
   return (
-    <section>
-        hello world
+    <section className='bg-blue-200 px-8'>
+    <h1 className='text-center text-3xl font-semibold text-slate-500 py-10'>Let's go we are visit : {countries.length}</h1>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+            {
+                countries.map(city => <SingleCountry 
+                city={city}
+                ></SingleCountry>)
+            }
+        </div>
     </section>
   )
 }
--- src/Components/SingleCountry.js
+++ src/Components/SingleCountry.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const SingleCountry = () => {
+const SingleCountry = (props) => {
+  console.log(props)
   return (
-    <div>
-        
+    <div className='bg-slate-200 p-4 rounded-md flex flex-col justify-center items-center shadow-md shadow-green-300 hover:scale-105 duration-300'>
+      
+        <img className='w-56 h-40' src={props.city.flags.png} alt="" />
+        <h1>Country: {props.city.name.common}</h1>
+        <p>Population: {props.city.population}</p>
     </div>
   )
 }
